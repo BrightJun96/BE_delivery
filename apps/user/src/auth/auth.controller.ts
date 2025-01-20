582dfefd3892b6f80211326667263444122a2c1e
jjalseu <cjfwns966@gmail.com>
2025-01-21 00:19:28 +0900

    user 서비스

---
+++ apps/user/src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Authorization } from './decorator/authorization.decorator';
+import { RegisterUserDto } from './dto/register-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async registerUser(
+    @Authorization() token: string,
+    @Body() registerUserDto: RegisterUserDto,
+  ) {
+    if (token === null) {
+      throw new UnauthorizedException('토큰을 입력해주세요.');
+    }
+
+    return await this.authService.register(token, registerUserDto);
+  }
+}
---
+++ apps/user/src/auth/dto/update-auth.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { RegisterUserDto } from './register-user.dto';
+
+export class UpdateAuthDto extends PartialType(RegisterUserDto) {
+  id: number;
+}
