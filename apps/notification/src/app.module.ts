0eb80b95b65f98b52b0142ba3b44b52d432552ab
jjalseu <cjfwns966@gmail.com>
2025-01-21 15:34:56 +0900

    notification service

--- apps/notification/src/app.module.ts
+++ apps/notification/src/app.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
-import { NotificationController } from './notification/notification.controller';
 
 import { NotificationModule } from './notification/notification.module';
-import { NotificationService } from './notification/notification.service';
 
 @Module({
   imports: [
@@ -27,7 +25,5 @@ import { NotificationService } from './notification/notification.service';
     }),
     NotificationModule,
   ],
-  controllers: [NotificationController],
-  providers: [NotificationService],
 })
 export class AppModule {}
