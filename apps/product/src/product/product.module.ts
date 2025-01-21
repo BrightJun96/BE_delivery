ecc13c04d2a6da9d239c933008f892f09a01adba
jjalseu <cjfwns966@gmail.com>
2025-01-21 13:31:46 +0900

    product:sample data

--- apps/product/src/product/product.module.ts
+++ apps/product/src/product/product.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
   providers: [ProductService],
 })
--- apps/product/src/product/product.service.ts
+++ apps/product/src/product/product.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async createSamples() {
+    const data = [
+      {
+        name: '사과',
+        price: 3000,
+        description: '씨없는 사과',
+        stock: 100,
+      },
+      {
+        name: '오렌지',
+        price: 2000,
+        description: '신 맛나는 오렌지',
+        stock: 500,
+      },
+      {
+        name: '메론',
+        price: 5000,
+        description: '껍질 없는 메론',
+        stock: 100,
+      },
+      {
+        name: '브로콜리',
+        price: 5000,
+        description: '마없음',
+        stock: 100,
+      },
+      {
+        name: '바나나',
+        price: 1000,
+        description: '마없음',
+        stock: 100,
+      },
+    ];
+
+    return await this.productRepository.save(data);
   }
 }
