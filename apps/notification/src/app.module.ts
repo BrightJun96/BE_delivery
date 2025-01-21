11c7ffb642d79adc0864d4258565088a26691fe0
jjalseu <cjfwns966@gmail.com>
2025-01-21 15:34:03 +0900

    notification service

---
+++ apps/notification/src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+import { NotificationController } from './notification/notification.controller';
+
+import { NotificationModule } from './notification/notification.module';
+import { NotificationService } from './notification/notification.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    NotificationModule,
+  ],
+  controllers: [NotificationController],
+  providers: [NotificationService],
+})
+export class AppModule {}
