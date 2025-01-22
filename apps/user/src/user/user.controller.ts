31ceeff5bae3d6a385bd9ffb8d5032b75108a330
jjalseu <cjfwns966@gmail.com>
2025-01-23 00:09:00 +0900

    grpc : user microservice => grpc

--- apps/user/src/user/user.controller.ts
+++ apps/user/src/user/user.controller.ts
@@ -1,24 +1,12 @@
-import { RpcInterceptor } from '@app/common';
-import {
-  Controller,
-  UseInterceptors,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { GetUserInfo } from './dto/get-user-info.dto';
+import { UserMicroservice } from '@app/common';
+import { Controller } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller()
-export class UserController {
+export class UserController implements UserMicroservice.UserServiceController {
   constructor(private readonly userService: UserService) {}
 
-  @MessagePattern({
-    cmd: 'get_user_info',
-  })
-  @UsePipes(ValidationPipe)
-  @UseInterceptors(RpcInterceptor)
-  getUserInfo(@Payload() getUserInfo: GetUserInfo) {
+  getUserInfo(getUserInfo: UserMicroservice.GetUserInfoRequest) {
     return this.userService.getUserById(getUserInfo.userId);
   }
 }
